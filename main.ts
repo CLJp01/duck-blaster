761ef921fa30ef888919b1ed698125d2f8ac7ff3
CLJp01 <89198321+CLJp01@users.noreply.github.com>
2023-10-12 16:34:41 +0900

    Added player move and fire laser controls.

--- main.ts
+++ main.ts
@@ -1,1 +1,43 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    laser = sprites.createProjectileFromSprite(img`
+        . . b b b b . . 
+        . b 5 5 5 5 b . 
+        b 5 d 3 3 d 5 b 
+        b 5 3 5 5 1 5 b 
+        c 5 3 5 5 1 d c 
+        c d d 1 1 d d c 
+        . f d d d d f . 
+        . . f f f f . . 
+        `, rocketShip, 100, 0)
+    laser.setFlag(SpriteFlag.DestroyOnWall, true)
+})
+let laser: Sprite = null
+let rocketShip: Sprite = null
+rocketShip = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+    b d d d b b d 5 5 4 4 4 4 4 b . 
+    b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+    c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+    c b d c d 5 5 b 5 5 5 5 5 5 b . 
+    . c d d c c b d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+rocketShip.setScale(1.5, ScaleAnchor.Middle)
+rocketShip.setPosition(20, scene.screenHeight() / 2)
+game.onUpdate(function () {
+    if (controller.up.isPressed()) {
+        rocketShip.y += -1
+    }
+    if (controller.down.isPressed()) {
+        rocketShip.y += 1
+    }
+})
